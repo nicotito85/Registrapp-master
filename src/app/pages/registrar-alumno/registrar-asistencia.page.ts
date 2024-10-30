dcfbc5607a87a34fcd87a6a38cb685adee47345f
nicotito85 <nicolasiisotoz@live.com>
2024-10-30 03:54:54 -0300

    update 2

--- src/app/pages/registrar-alumno/registrar-asistencia.page.ts
+++ src/app/pages/registrar-alumno/registrar-asistencia.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 
 @Component({
   selector: 'app-registrar-asistencia',
@@ -7,10 +10,34 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./registrar-asistencia.page.scss'],
 })
 export class RegistrarAsistenciaPage implements OnInit {
+  asignaturas: any[] = [];
 
-  constructor(private navCtrl: NavController) { }
 
-    
+  constructor(private navCtrl: NavController,
+    private afAuth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) { }
+
+  async loadAsignaturas() {
+    const user = await this.afAuth.currentUser;
+    if (user) {
+      const alumnoDoc = await this.firestore.collection('Alumnos').doc(user.uid).get().toPromise();
+      const alumnoData = alumnoDoc?.data() as any;
+
+      if (alumnoData) {
+        const siglas = [alumnoData.sigla1, alumnoData.sigla2, alumnoData.sigla3];
+        const asignaturasSnapshot = await this.firestore
+          .collection('Asignaturas', ref => ref.where('sigla', 'in', siglas))
+          .get()
+          .toPromise();
+
+        this.asignaturas = asignaturasSnapshot?.docs.map(doc => doc.data()) || [];
+      } else {
+        console.error('No se encontró el alumno en la colección Alumnos.');
+      }
+    }
+  }
+
   navigateToPerfil() {
     this.navCtrl.navigateForward('/perfil-alumno');
   }
@@ -30,8 +57,9 @@ export class RegistrarAsistenciaPage implements OnInit {
   navigateToQRScan() {
     this.navCtrl.navigateForward('/qr-alumn');}
 
-
-  ngOnInit() {
+ ngOnInit() {
+    this.loadAsignaturas();
   }
+
 
 }
--- src/app/pages/revisar-alumno/revisar-asistencia.page.ts
+++ src/app/pages/revisar-alumno/revisar-asistencia.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-revisar-asistencia',
@@ -7,9 +9,34 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./revisar-asistencia.page.scss'],
 })
 export class RevisarAsistenciaPage implements OnInit {
+  asignaturas: any[] = [];
+  constructor(
+    private navCtrl: NavController,
+    private firestore: AngularFirestore,
+    private afAuth: AngularFireAuth
+  ) {}
 
-  constructor(private navCtrl: NavController) { }
-  
+
+  async loadAsignaturas() {
+    const user = await this.afAuth.currentUser;
+    if (user) {
+      const alumnoDoc = await this.firestore.collection('Alumnos').doc(user.uid).get().toPromise();
+      const alumnoData = alumnoDoc?.data() as any;
+
+      if (alumnoData) {
+        const siglas = [alumnoData.sigla1, alumnoData.sigla2, alumnoData.sigla3];
+        const asignaturasSnapshot = await this.firestore
+          .collection('Asignaturas', ref => ref.where('sigla', 'in', siglas))
+          .get()
+          .toPromise();
+
+        this.asignaturas = asignaturasSnapshot?.docs.map(doc => doc.data()) || [];
+      } else {
+        console.error('No se encontró el alumno en la colección Alumnos.');
+      }
+    }
+  }
+
     
   navigateToPerfil() {
     this.navCtrl.navigateForward('/perfil-alumno');
@@ -30,6 +57,8 @@ export class RevisarAsistenciaPage implements OnInit {
 
   
   ngOnInit() {
+    this.loadAsignaturas();
   }
+
 
 }
